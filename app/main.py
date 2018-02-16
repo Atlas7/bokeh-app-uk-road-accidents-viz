70d15e079614b7efb73202e80b056cacf358eb08
Atlas7 <johnnychan0302@gmail.com>
2018-02-16 15:53:17 +0000

    add licence and update readme

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
 """
-Interactive Big Data Visualization Web Application
-by Johnny Chan (Github: Atlas7)
+Bokeh Web Application for Big Data Visualization - with HoloViews and Datashader
+===
 
-This is an interactive web application tool that enables big data visualization of the 136k+
-UK Road Accidents reported during 2016.
+MIT License
 
-For more information about the datasets please refer to
-[the Road Safety Data Page](https://data.gov.uk/dataset/road-accidents-safety-data), which contains datasets
-for other periods and dimensions.
+Copyright (c) 2018 Johnny Chan
 
-This work is inspired by the [pyviz](https://github.com/pyviz/pyviz) tutorial series.
-It is my hope to incorporate further enhancements to this application, refactor and optimise the codes, 
-and use as a reference baseline template for building other similar applications with even better capabilities.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-Built with Anaconda Data Science Tools: Bokeh, Datashader, HoloViews, GeoViews, Dask, Pandas
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 import pandas as pd
@@ -29,7 +38,6 @@ from bokeh.tile_providers import STAMEN_TERRAIN, STAMEN_TONER
 # from bokeh.io import curdoc
 from utils import cm, get_plot_params
 # from hv.streams import RangeXY
-
 import warnings
 
 # Ignore the excessive repeated warnings. Comment this out if you want warnings.
